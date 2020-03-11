fb4047e27a7917415c79642d74395e4a55cecc48
Jiangqiaoqiao <jiangqiao@whut.edu.cn>
2020-03-11 11:46:30 +0800

    fourth update Cnn_Autoencoder

--- Cnn_Autoencoder/Cnn_Autoencoder/cnn_AE_2.py
+++ Cnn_Autoencoder/Cnn_Autoencoder/cnn_AE_2.py
@@ -5,82 +5,72 @@ import tensorflow as tf
 from tensorflow.keras import layers, optimizers
 from tensorflow.keras.models import Model, load_model
 from tensorflow.keras.layers import Conv1D, Conv2D, BatchNormalization, \
-    Input, UpSampling2D, Lambda, Conv2DTranspose, Activation
+    Input, UpSampling2D, Lambda, Conv2DTranspose, Activation, ZeroPadding2D,\
+    Cropping2D
 # from keras.layers.advanced_activations import LeakyReLU
 import keras.optimizers
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 
-def abMaxPooling1D(inputs, pool_size=2, strides=2, padding='SAME'):
-    # output1 = MaxPooling1D(pool_size=pool_size, strides=strides, padding=padding)(inputs)
-    # output2 = MaxPooling1D(pool_size=pool_size, strides=strides, padding=padding)(-inputs)
-    output1 = tf.nn.max_pool1d(inputs, ksize=pool_size, strides=strides, padding=padding)
-    output2 = tf.nn.max_pool1d(-inputs, ksize=pool_size, strides=strides, padding=padding)
-    mask = output1 >= output2
-    output = tf.where(mask, output1, -output2)
-    return output
-
-def abMaxPooling2D(inputs, pool_size=[2, 2], strides=2, padding='SAME'):
-    # output1 = MaxPooling2D(pool_size=pool_size, strides=strides, padding=padding)(inputs)
-    # output2 = MaxPooling2D(pool_size=pool_size, strides=strides, padding=padding)(-inputs)
-    output1 = tf.nn.max_pool2d(inputs, ksize=pool_size, strides=strides, padding=padding)
-    output2 = tf.nn.max_pool2d(-inputs, ksize=pool_size, strides=strides, padding=padding)
-    mask = output1 >= output2
-    output = tf.where(mask, output1, -output2)
-    return output
+# def abMaxPooling1D(inputs, pool_size=2, strides=2, padding='SAME'):
+#     # output1 = MaxPooling1D(pool_size=pool_size, strides=strides, padding=padding)(inputs)
+#     # output2 = MaxPooling1D(pool_size=pool_size, strides=strides, padding=padding)(-inputs)
+#     output1 = tf.nn.max_pool1d(inputs, ksize=pool_size, strides=strides, padding=padding)
+#     output2 = tf.nn.max_pool1d(-inputs, ksize=pool_size, strides=strides, padding=padding)
+#     mask = output1 >= output2
+#     output = tf.where(mask, output1, -output2)
+#     return output
+#
+# def abMaxPooling2D(inputs, pool_size=[2, 2], strides=2, padding='SAME'):
+#     # output1 = MaxPooling2D(pool_size=pool_size, strides=strides, padding=padding)(inputs)
+#     # output2 = MaxPooling2D(pool_size=pool_size, strides=strides, padding=padding)(-inputs)
+#     output1 = tf.nn.max_pool2d(inputs, ksize=pool_size, strides=strides, padding=padding)
+#     output2 = tf.nn.max_pool2d(-inputs, ksize=pool_size, strides=strides, padding=padding)
+#     mask = output1 >= output2
+#     output = tf.where(mask, output1, -output2)
+#     return output
 
 def abMaxPooling_with_argmax(inputs, pool_size=2, strides=2, padding='SAME'):
+
     output1, argmax1 = tf.nn.max_pool_with_argmax(inputs, ksize=pool_size, strides=strides, padding=padding)
     output2, argmax2 = tf.nn.max_pool_with_argmax(-inputs, ksize=pool_size, strides=strides, padding=padding)
+    argmax1 = tf.stop_gradient(argmax1)
+    argmax2 = tf.stop_gradient(argmax2)
+    # output1 = tf.nn.max_pool2d(inputs, ksize=pool_size, strides=strides, padding=padding)
+    # output2 = tf.nn.max_pool2d(-inputs, ksize=pool_size, strides=strides, padding=padding)
     mask = output1 >= output2
     output = tf.where(mask, output1, -output2)
     argmax = tf.where(mask, argmax1, argmax2)
     return (output, argmax)
 
 
-def unAbMaxPooling(inputs, argmax, strides):
-    """
-        input_tensor: unAbMaxPooling后的output tensor
-        argmax：之前AbMaxPooling后最大值的index tensor
-        strides：池化步长
-        """
-    #  计算input_shape
-    input_shape = [x for x in inputs.get_shape()]  # [1, ,2, 2, 2]
-    #  计算output_shape
-    output_shape = [input_shape[0], input_shape[1] * strides, input_shape[2] * strides,
-                    input_shape[3]]  # [1, 4, 4, 2]
-
-    """
-    The indices in argmax are flattened, so that a maximum value at position [b, y, x, c] becomes 
-    flattened index ((b * height + y) * width + x) * channels + c
-    """
-    # 计算 b, y, x, c
-    batch_range = tf.reshape(tf.range(output_shape[0], dtype=tf.int64),
-                             shape=[input_shape[0], 1, 1, 1])  # batch index range
-    # [1, 1, 1, 1]
-    channel_range = tf.range(output_shape[3], dtype=tf.int64)
-
-    b = tf.ones_like(argmax) * batch_range  # [1, 2, 2, 2]  广播
-
-    y = argmax // (output_shape[2] * output_shape[3])  # [1, 2, 2, 2]  地板除以 width * channels ，不用管x， x一定比width小
-
-    x = argmax % (output_shape[2] * output_shape[3]) // output_shape[3]  # [1, 2, 2, 2]
-
-    c = tf.ones_like(argmax) * channel_range  # [1, 2, 2, 2] 广播
-
-    # 计算input_tensor的元素个数
-    updates_size = tf.size(inputs)  # 8
-
-    # 将b, y, x, c堆叠起来 构成一个新 tensor ，维度 + 1
-    # 索引矩阵 跟 b 对齐再转置
-    indices = tf.transpose(tf.reshape(tf.stack([b, y, x, c]), [4, updates_size]))  # [8, 4]  4为 size of [b, y, x, c]
-    # max数组
-    values = tf.reshape(inputs, [updates_size, ])  # [8]
-
-    # 重构 output 先构建全0的Tensor 然后value根据index中提供的坐标找到自己的位置
-    outputs = tf.tensor_scatter_nd_add(tf.zeros(output_shape, values.dtype), indices, values)
-
+def unAbMaxPooling(inputs_argmax, ksize, strides=2, padding='SAME'):
+    # 假定 ksize = strides
+    inputs = inputs_argmax[0]
+    argmax = inputs_argmax[1]
+    input_shape = inputs.get_shape()
+    if padding == 'SAME':
+        rows = input_shape[1] * ksize[1]
+        cols = input_shape[2] * ksize[2]
+    else:
+        rows = (input_shape[1] - 1) * ksize[1] + ksize[1]
+        cols = (input_shape[2] - 1) * ksize[2] + ksize[2]
+    # 计算new shape
+    output_shape = (input_shape[0], rows, cols, input_shape[3])
+    # 计算索引
+    one_like_mask = tf.ones_like(argmax)
+    batch_range = tf.reshape(tf.range(output_shape[0], dtype=tf.int64), shape=[input_shape[0], 1, 1, 1])
+    b = one_like_mask * batch_range
+    y = argmax // (output_shape[2] * output_shape[3])
+    x = argmax % (output_shape[2] * output_shape[3]) // output_shape[3]
+    feature_range = tf.range(output_shape[3], dtype=tf.int64)
+    c = one_like_mask * feature_range
+    # 转置索引
+    update_size = tf.size(inputs)
+    indices = tf.transpose(tf.reshape(tf.stack([b, y, x, c]), [4, update_size]))
+    values = tf.reshape(inputs, [update_size])
+    outputs = tf.scatter_nd(indices, values, output_shape)
     return outputs
 
 
@@ -103,72 +93,78 @@ class Cnn_AE_2:
 
 
     def build_model(self, input_shape):
-        input_layer = Input(shape=input_shape)
-        input_layer = Input(shape=(32, 576, 1))
+        input_layer = Input(batch_shape=(1000, input_shape[0], input_shape[1], input_shape[2]))
+        # input_layer = Input(batch_shape=(6, 32, 576, 1))
+        # h1 = input_layer.get_shape()[1]
+        # input_layer = Conv2D(filters=16, kernel_size=(h1, 3), padding='same')
         # Encoder
         # conv block -1 （卷积+池化）
-        if len(input_layer.shape) == 3:
-            # conv1 = ZeroPadding1D(2)(input_layer)
-            conv1 = Conv1D(filters=16, kernel_size=5, padding='same')(input_layer)
-            conv1 = BatchNormalization()(conv1)
-            conv1 = Activation(activation='relu')(conv1)
-            conv1_pool = Lambda(abMaxPooling1D, arguments={'pool_size': 2})(conv1)
-            conv1_pool = Lambda(reshapes, arguments={'retype': 'reshapedim'})(conv1_pool)
-        if len(input_layer.shape) == 4:
-            conv1 = Conv2D(filters=16, kernel_size=(3, 5), padding='same')(input_layer)
-            conv1 = BatchNormalization()(conv1)
-            conv1 = Activation(activation='relu')(conv1)
-            # conv1_pool, conv1_argmax = Lambda(abMaxPooling_with_argmax, arguments={'pool_size': [2, 2]}, name='abMaxPool1')(conv1)
-            conv1_pool = Lambda(abMaxPooling2D, arguments={'pool_size': [2, 2]}, name='abMaxPool1')(conv1)
+        # if len(input_layer.shape) == 3:
+        #     # conv1 = ZeroPadding1D(2)(input_layer)
+        #     conv1 = Conv1D(filters=16, kernel_size=5, padding='same')(input_layer)
+        #     conv1 = BatchNormalization()(conv1)
+        #     conv1 = Activation(activation='relu')(conv1)
+        #     conv1_pool = Lambda(abMaxPooling1D, arguments={'pool_size': 2})(conv1)
+        #     conv1_pool = Lambda(reshapes, arguments={'retype': 'reshapedim'})(conv1_pool)
+        # if len(input_layer.shape) == 4:
+        conv1 = Conv2D(filters=16, kernel_size=(3, 5), padding='same')(input_layer)
+        conv1 = BatchNormalization()(conv1)
+        conv1 = Activation(activation='relu')(conv1)
+        conv1_pool, conv1_argmax = Lambda(abMaxPooling_with_argmax, arguments={'pool_size': [2, 2]}, name='abMaxPool1')(conv1)
+        # conv1_pool = Lambda(abMaxPooling2D, arguments={'pool_size': [2, 2]}, name='abMaxPool1')(conv1)
+
 
         # conv block -2 （卷积+池化）
         conv2 = Conv2D(filters=8, kernel_size=(3, 5), padding='same')(conv1_pool)
         conv2 = BatchNormalization()(conv2)
         conv2 = Activation(activation='relu')(conv2)
-        # conv2_pool, conv2_argmax = Lambda(abMaxPooling_with_argmax, arguments={'pool_size': [2, 2]}, name='abMaxPool2')(conv2)
-        conv2_pool = Lambda(abMaxPooling2D, arguments={'pool_size': [2, 2]}, name='abMaxPool2')(conv2)
+        conv2_pool, conv2_argmax = Lambda(abMaxPooling_with_argmax, arguments={'pool_size': [2, 2]}, name='abMaxPool2')(conv2)
+        # conv2_pool = Lambda(abMaxPooling2D, arguments={'pool_size': [2, 2]}, name='abMaxPool2')(conv2)
 
         # conv block -3 （卷积）
         conv3 = Conv2D(filters=8, kernel_size=(3, 5), padding='same')(conv2_pool)
         conv3 = BatchNormalization()(conv3)
         conv3 = Activation(activation='relu')(conv3)
-        # encoder, conv3_argmax = Lambda(abMaxPooling_with_argmax, arguments={'pool_size': [2, 2]}, name='abMaxPool3')(conv3)
-        encoder = Lambda(abMaxPooling2D, arguments={'pool_size': [2, 2]}, name='abMaxPool3')(conv3)
+        encoder, conv3_argmax = Lambda(abMaxPooling_with_argmax, arguments={'pool_size': [2, 2]}, name='abMaxPool3')(conv3)
+        # encoder = Lambda(abMaxPooling2D, arguments={'pool_size': [2, 2]}, name='abMaxPool3')(conv3)
 
 
         # decoder
         # conv block -1 （反卷积+反池化）
-        deconv1_unpool = UpSampling2D(size=(2, 2))(encoder)
-        # deconv1_unpool = Lambda(unAbMaxPooling, arguments={'argmax': conv3_argmax, 'strides': 2}, name='unAbPool1')(encoder)
-        deconv1 = Conv2DTranspose(filters=8, kernel_size=(3, 5), padding='same')(deconv1_unpool)
+        # deconv1_unpool = UpSampling2D(size=(2, 2))(encoder)
+        deconv1 = Conv2DTranspose(filters=8, kernel_size=(3, 5), padding='same')(encoder)
         deconv1 = BatchNormalization()(deconv1)
         deconv1 = Activation(activation='relu')(deconv1)
-
+        deconv1_unpool = Lambda(unAbMaxPooling, arguments={'ksize': [1, 2, 2, 1]}, name='unAbPool1')([deconv1, conv3_argmax])
+        # 剪掉部分
+        deconv1_unpool = Cropping2D(cropping=((0, 1), (0, 1)))(deconv1_unpool)
         # conv block -2 （反卷积+反池化）
-        deconv2_unpool = UpSampling2D(size=(2, 2))(deconv1)
-        # deconv2_unpool = Lambda(unAbMaxPooling, arguments={'argmax': conv2_argmax, 'strides': 2}, name='unAbPool2')(deconv1)
-        deconv2 = Conv2DTranspose(filters=8, kernel_size=(3, 5), padding='same')(deconv2_unpool)
+        # deconv2_unpool = UpSampling2D(size=(2, 2))(deconv1)
+        deconv2 = Conv2DTranspose(filters=8, kernel_size=(3, 5), padding='same')(deconv1_unpool)
         deconv2 = BatchNormalization()(deconv2)
         deconv2 = Activation(activation='relu')(deconv2)
+        deconv2_unpool = Lambda(unAbMaxPooling, arguments={'ksize': [1, 2, 2, 1]}, name='unAbPool2')([deconv2, conv2_argmax])
 
         # conv block -3 （反卷积+反池化）
-        deconv3_unpool = UpSampling2D(size=(2, 2))(deconv2)
-        # deconv3_unpool = Lambda(unAbMaxPooling, arguments={'argmax': conv1_argmax, 'strides': 2}, name='unAbPool3')(deconv2)
-        deconv3 = Conv2DTranspose(filters=16, kernel_size=(3, 5), padding='same')(deconv3_unpool)
+        # deconv3_unpool = UpSampling2D(size=(2, 2))(deconv2)
+        deconv3 = Conv2DTranspose(filters=16, kernel_size=(3, 5), padding='same')(deconv2_unpool)
         deconv3 = BatchNormalization()(deconv3)
         deconv3 = Activation(activation='relu')(deconv3)
+        deconv3_unpool = Lambda(unAbMaxPooling, arguments={'ksize': [1, 2, 2, 1]}, name='unAbPool3')([deconv3, conv1_argmax])
 
         # decoder = Conv2D(filters=1, kernel_size=(16, 5), activation='relu', padding='same')(deconv3_unpool)
-        output_layer = Conv2DTranspose(filters=input_shape[2], kernel_size=(3, 5), padding='same')(deconv3)
+        output_layer = Conv2DTranspose(filters=input_shape[2], kernel_size=(3, 5), padding='same')(deconv3_unpool)
         output_layer = BatchNormalization()(output_layer)
-        output_layer = Activation(activation='relu')(output_layer)
+        output_layer = Activation(activation='sigmoid')(output_layer)
 
         model = Model(inputs=input_layer, outputs=output_layer)
 
         # model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
-        model.compile(loss='mse', optimizer=optimizers.Adam(0.001), metrics=['mse'])
+        model.compile(loss='mse',
+                      optimizer=optimizers.Adam(0.001),
+                      metrics=['mse'])
 
-        file_path = os.path.join(self.output_directory, 'best_model.hdf5')
+        file_path = os.path.join(self.output_directory, 'cnn_AE_2/best_model.hdf5')
 
         log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
         # tensorboard = tf.keras.callbacks.Tensorboard(log_dir=log_dir)
@@ -183,26 +179,28 @@ class Cnn_AE_2:
         return model
 
     def fit_model(self, x_train, y_train, x_val, y_val):
-        # x_val and y_val are only used to monitor the test loss and NOT for training
-        batch_size = 12
-        nb_epochs = 100
+        batch_size = 1000
+        nb_epochs = 10
 
         # 小批量训练大小
-        mini_batch_size = int(min(x_train.shape[0] / 10, batch_size))
+        # mini_batch_size = int(min(x_train.shape[0] / 10, batch_size))
 
         start_time = time.time()
 
         # 训练模型
-        hist = self.model.fit(x_train, y_train, batch_size=mini_batch_size, epochs=nb_epochs,
-                       verbose=self.verbose, validation_data=(x_val, y_val), callbacks=self.callbacks, validation_freq=4)
+        hist = self.model.fit(x_train, y_train,
+                              epochs=nb_epochs,
+                              verbose=self.verbose,
+                              validation_data=(x_val, y_val),
+                              callbacks=self.callbacks)
 
         duration = time.time() - start_time
         print(duration)
 
         # 做测试，所以需要加载模型
-        model = load_model(os.path.join(self.output_directory, 'best_model.hdf5'))
+        model = load_model(os.path.join(self.output_directory, 'cnn_AE_2/best_model.hdf5'))
 
-        loss = model.evaluate(x_val, y_val, batch_size=mini_batch_size, verbose=0)
+        loss = model.evaluate(x_val, y_val, batch_size=batch_size, verbose=0)
         # y_pred = model.predict(x_val)
         # y_pred = np.argmax(y_pred, axis=1)
         print('test_loss: ', loss)
--- Cnn_Autoencoder/Cnn_Autoencoder/main.py
+++ Cnn_Autoencoder/Cnn_Autoencoder/main.py
@@ -1,18 +1,25 @@
 from  cnn_AE_1 import Cnn_AE_1
+from cnn_AE_1pro import Cnn_AE_1pro
 import tensorflow as tf
 import numpy as np
 from cnn_AE_2 import Cnn_AE_2
+from tensorflow_core.examples.tutorials.mnist import input_data
 
 def readucr(filename):#读取数据
     data = np.loadtxt(filename, dtype=str, delimiter = ',')
     x = data[:, 0:576]
     return x
 
+    # mnist = input_data.read_data_sets(filename, one_hot=True)
+    # trainimg = mnist.train.images
+    # testimg = mnist.test.images
+    # return trainimg, testimg
 
-def preprocess(x_train, x_test):
-    train_dataset = tf.data.Dataset.from_tensor_slices((x_train, x_train))
-    test_dataset = tf.data.Dataset.from_tensor_slices((x_test, x_test))
-    return train_dataset, test_dataset
+
+# def preprocess(x_train, x_test):
+#     train_dataset = tf.data.Dataset.from_tensor_slices((x_train, x_train))
+#     test_dataset = tf.data.Dataset.from_tensor_slices((x_test, x_test))
+#     return train_dataset, test_dataset
 
 
 if __name__ == '__main__':
@@ -30,21 +37,21 @@ if __name__ == '__main__':
     x_test = x_test.astype(float)
     x_test = tf.cast(x_test, dtype=tf.float32)
     # train_dataset, test_dataset = preprocess(x_train, x_test)
-    input_shape = (1, x_train.shape[2], 1)
-    cnn_Auto = Cnn_AE_1('result', input_shape, True)
+    input_shape = (1, x_train.get_shape()[2], 1)
+
+    # 图片数据测试
+    # x_train, x_test = readucr(r'data/mnist/')
+    # x_train = np.reshape(x_train, (-1, 28, 28))
+    # x_train = np.expand_dims(x_train, -1)
+    # x_test = np.reshape(x_test, (-1, 28, 28))
+    # x_test = np.expand_dims(x_test, -1)
+    # input_shape = (28, 28, 1)
+
+    # cnn_Auto = Cnn_AE_1('result', input_shape, True)
+    cnn_Auto = Cnn_AE_1pro('result', input_shape, True)
     # cnn_Auto = Cnn_AE_2('result', input_shape, True)
     cnn_Auto.fit_model(x_train, x_train, x_test, x_test)
     # cnn_Auto.fit_model(train_dataset, test_dataset)
 
 
 
-
-
-
-    # x_train = data_Process.readucr('data/Car_TRAIN.txt', 1)  # 每一个是577个数据，一类有16个
-    # x_train = np.array(x_train)
-    # print(x_train.shape)
-    # x_test = data_Process.readucr('data/Car_TEST.txt', 1)[0]
-    # x_val = data_Process.readucr('data/Car_TEST.txt', 1)[1]
-    # cnn_Auto = Cnn_AE_1('result', (len(x_train),), True)
-    # cnn_Auto.train_model(x_train, x_train, x_val, x_val)
