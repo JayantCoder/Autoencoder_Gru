a2030fa080da6b53d55b64538a921e46924d12cb
NoTargetFish <1102971102@qq.com>
2020-04-12 20:19:24 +0800

    main函数训练模型，abnormal_detect做异常检测

--- Cnn_Autoencoder/main.py
+++ Cnn_Autoencoder/main.py
@@ -70,22 +70,8 @@ if __name__ == '__main__':
     cnn_Auto = Cnn_AE_n('result', input_shape, 6, True)
     cnn_Auto.fit_model(gru_train_concat, gru_train_concat, gru_test_concat, gru_test_concat, epochs=2)
 
-    # 输入测试数据
-    test_data, _, _, _ = readUcrTsv('data/CinCECGTorso/test_data.tsv', timeWinNum, method=2)
-    g_test = {}
-    for i in range(sensors_num):
-        # GruFilepath['model_' + str(i)] = 'result/multigru/cnn_AE_n/CinCECGTorso_gru_model_' + str(i) + '.hdf5'
-        GruFilepath['model_' + str(i)] = gru_model['model_' + str(i)].model_path
-        g_test['model_' + str(i)] = output_of_multi_gru(test_data[i], GruFilepath['model_' + str(i)],
-                                                           timewindows=timewindows)
-        print(g_test['model_' + str(i)].shape)
-    g_test_all = [g_test['model_' + str(i)] for i in range(sensors_num)]
-    g_test_concat = tf.concat(g_test_all, axis=1)
-    print(g_test_concat.shape)
 
-    # 测试是否异常
-    # cnnfilepath = cnn_Auto.model_path
-    is_abnormal('result/cnn_AE_n/cnn_model.hdf5', g_test_concat[:1], gru_train_concat, gru_train_concat)
+
 
 
 
